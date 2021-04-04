b026dca66e6858d940a951faa5d49fa7e180713c
Alex O'Brien <3541ax@gmail.com>
2021-04-04 12:19:31 +1000

    install: Add git.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 cd $(dirname $(readlink -f "$0"))
 
-for d in bash emacs i3 nvim sakura redshift; do
+for d in bash emacs i3 nvim sakura redshift git; do
     echo "$d"
     stow "$d"
 done
