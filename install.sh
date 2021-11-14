524e4b5fe0a7e8585fdb617330684dd94941a5d6
Alex O'Brien <3541ax@gmail.com>
2021-11-14 14:20:23 +1100

    nix: Set up current.nix symlink.

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ cd $(dirname $(readlink -f "$0"))
 
 if command -v nix &> /dev/null; then
     nix-shell -p stow --command 'stow nix'
+    if [ ! -f nix/.config/nixpkgs/machines/current.nix ]; then
+        ln -s $(hostname).nix nix/.config/nixpkgs/machines/current.nix
+    fi
+
     if ! command -v home-manager &> /dev/null; then
         sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
         sudo nix-channel --update
