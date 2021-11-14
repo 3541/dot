5160e4cb1ec75e0214190395a906b7934d181e18
Alex O'Brien <3541ax@gmail.com>
2021-11-14 13:18:04 +1100

    nix: Add spirit.

---
+++ nix/.config/nixpkgs/machines/spirit.nix
@@ -0,0 +1,30 @@
+{
+  a3 = {
+    enable = true;
+    platform = "linux";
+    displayServer = "wayland";
+    potato = true;
+    fontSize = 18.0;
+  };
+
+  wayland.windowManager.sway.config = {
+    output = {
+      "eDP-1" = {
+        resolution = "3840x2160";
+        scale = "1.0";
+      };
+      "HDMI-A-1" = {
+        resolution = "1280x720";
+        pos = "0 0";
+        scale = "0.5";
+      };
+    };
+
+    input = {
+      "type:touchpad".events = "disabled";
+      "type:touch".events = "disabled";
+      "type:tablet tool".events = "disabled";
+      "type:pointer".pointer_accel = "1.0";
+    };
+  };
+}
