936bd30cc7ab49212e3abc58f199719ca9f1f877
Alex O'Brien <3541ax@gmail.com>
2021-11-21 14:28:13 +1100

    nix: Opportunity monitor changes.

--- nix/.config/nixpkgs/machines/opportunity.nix
+++ nix/.config/nixpkgs/machines/opportunity.nix
@@ -9,10 +9,12 @@
     };
 
     xsession.windowManager.i3.config.startup = [{
-      command = "xrandr --output DP-2 --mode 3840x2160 --right-of DVI-D-0";
+      command =
+        "xrandr --output DP-4 --mode 3840x2160 --left-of DP-0 --output DP-0 --left-of DVI-D-0";
       always = true;
     }];
-    xsession.windowManager.i3.config.keybindings."Mod4+Shift+o" = "exec xrandr --output DVI-D-0 --off";
+    xsession.windowManager.i3.config.keybindings."Mod4+Shift+o" =
+      "exec xrandr --output DVI-D-0 --off --output DP-4 --off";
 
     programs.obs-studio.enable = true;
   };
