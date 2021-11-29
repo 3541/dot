7dd06e3c863ca6a6616756259743267eaeebb974
Alex O'Brien <3541ax@gmail.com>
2021-11-29 11:13:29 +1100

    nix: Disable gpg signing on sojourner.

--- nix/.config/nixpkgs/machines/sojourner.nix
+++ nix/.config/nixpkgs/machines/sojourner.nix
@@ -1,9 +1,11 @@
-{
+{ lib, ... }: {
   config = {
     a3 = {
       enable = true;
       platform = "macOS";
       userName = "aobrien";
     };
+
+    programs.git.signing.signByDefault = lib.mkForce false;
   };
 }
