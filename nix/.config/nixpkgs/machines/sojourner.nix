0f922a796f8e5c622bc6ff5f13d472d6a5b864c2
Alex O'Brien <3541ax@gmail.com>
2021-12-06 09:08:15 +1100

    nix: Aliases and scripts for sojourner.

--- nix/.config/nixpkgs/machines/sojourner.nix
+++ nix/.config/nixpkgs/machines/sojourner.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   config = {
     a3 = {
       enable = true;
@@ -7,11 +7,19 @@
       fontSize = 16.0;
       shExtra = ''
         export SPINUP_CONFIG="default_config/roadrunner_syd.sh"
-        alias imc='ssh sytes172'
+        alias imc='ssh -X sytes172'
+        alias ss='~/src/docker_spinup/spinup shell'
+        alias sst='~/src/docker_spinup/spinup stop'
       '';
     };
 
     programs.git.signing.signByDefault = lib.mkForce false;
     programs.git.userEmail = lib.mkForce "alex.obrien@imc.com";
+
+    home.packages = [
+      (pkgs.writeShellScriptBin "clangd" ''
+        /usr/local/bin/docker exec -i aobrien_v2_dev_container /opt/imc/llvm-11.0.0/bin/clangd --path-mappings=/Users/aobrien=/outside ''${@:1}
+      '')
+    ];
   };
 }
