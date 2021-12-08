217cd4e85b19e54798067ac8413f8fec2ef07dcd
Alex O'Brien <3541ax@gmail.com>
2021-12-08 18:21:42 +1100

    nix: Add /opt/local/bin to path on sojourner.

--- nix/.config/nixpkgs/machines/sojourner.nix
+++ nix/.config/nixpkgs/machines/sojourner.nix
@@ -6,6 +6,7 @@
       userName = "aobrien";
       fontSize = 16.0;
       shExtra = ''
+        export PATH="$PATH:/opt/local/bin"
         export SPINUP_CONFIG="default_config/roadrunner_syd.sh"
         alias imc='ssh -X sytes172'
         alias ss='~/src/docker_spinup/spinup shell'
