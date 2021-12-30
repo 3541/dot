7b367237622859f755e7199802441953962a9eb5
Alex O'Brien <3541ax@gmail.com>
2021-12-30 20:42:05 +1100

    nix: Enable fwupd.

--- nixos/src/hardware.nix
+++ nixos/src/hardware.nix
@@ -4,6 +4,7 @@ in {
   config = {
     hardware.cpu.intel.updateMicrocode = cfg.cpu == "intel";
     hardware.cpu.amd.updateMicrocode = cfg.cpu == "amd";
+    services.fwupd.enable = true;
 
     services.thermald.enable = cfg.formFactor == "portable" && cfg.cpu == "intel";
     powerManagement.powertop.enable = cfg.formFactor == "portable";
