35288a125d6f997347ded3c8cce9869744623e18
Alex O'Brien <3541ax@gmail.com>
2022-02-24 13:11:44 +1100

    Fix Wine version.

--- nix/.config/nixpkgs/src/programs/me3t.nix
+++ nix/.config/nixpkgs/src/programs/me3t.nix
@@ -2,7 +2,7 @@
   home.packages = with pkgs;
     [
       (writeShellScriptBin "me3t" ''
-        WINEPREFIX=/mass/games/me3t/wine ${wineWowPackages.staging}/bin/wine64 /mass/games/me3t/ME3TweaksModManager.exe
+        WINEPREFIX=/mass/games/me3t/wine ${wineWowPackages.base}/bin/wine64 /mass/games/me3t/ME3TweaksModManager.exe
       '')
     ];
 }
