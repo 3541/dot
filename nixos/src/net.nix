2c033c2fd6f34f59776355a9150db9544de382ff
Alex O'Brien <3541ax@gmail.com>
2022-03-15 13:47:38 +1100

    TIDY: Clean up net.nix.

--- nixos/src/net.nix
+++ nixos/src/net.nix
@@ -1,8 +1,8 @@
 { lib, config, ... }:
 let cfg = config.a3;
 in {
-  config = {
-    networking.hostName = cfg.hostname;
-    networking.networkmanager.enable = cfg.formFactor == "portable";
+  config.networking = {
+    hostName = cfg.hostname;
+    networkmanager.enable = cfg.formFactor == "portable";
   };
 }
