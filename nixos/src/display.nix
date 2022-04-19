79688743144409f56c7363ba63f8db6df0f5dc49
Alex O'Brien <3541ax@gmail.com>
2022-04-19 18:22:14 +1000

    Revert "Disable X11 libs when X is disabled."
    This reverts commit 938a6578b867970808c452507299040b8e6422f2.

--- nixos/src/display.nix
+++ nixos/src/display.nix
@@ -1,7 +1,7 @@
 { lib, config, pkgs, ... }:
 let cfg = config.a3;
 in {
-  config = if (cfg.displayServer != "none") then {
+  config = lib.mkIf (cfg.displayServer != "none") {
     services.xserver = {
       enable = true;
       videoDrivers = cfg.videoDrivers;
@@ -49,8 +49,12 @@ in {
           };
           ligations = {
             inherits = "dlig";
-            disables =
-              [ "brace-bar" "brack-bar" "connected-tilde-as-wave" "plusplus" ];
+            disables = [
+              "brace-bar"
+              "brack-bar"
+              "connected-tilde-as-wave"
+              "plusplus"
+            ];
           };
         };
         set = "custom";
@@ -60,7 +64,5 @@ in {
       font-awesome_4
       nerdfonts
     ];
-  } else {
-    environment.noXlibs = true;
   };
 }
