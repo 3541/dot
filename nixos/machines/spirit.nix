93b501e44b9075c153d9ba6dd95b4def002a4af6
Alex O'Brien <3541@3541.website>
2022-05-14 15:57:58 +1000

    spirit: Enable backups.

--- nixos/machines/spirit.nix
+++ nixos/machines/spirit.nix
@@ -19,6 +19,38 @@ in {
     networking.interfaces.enp0s31f6.useDHCP = true;
     networking.interfaces.wlp0s20f3.useDHCP = true;
 
+    services.borgbackup.jobs.backup = {
+      paths = [ "/home/alex" "/etc/nixos" ];
+      exclude = [
+        ".cache"
+        "*/Cache"
+        "*/cache"
+        "images"
+        ".local/share/Steam"
+        ".cargo"
+        ".config"
+        ".npm"
+        "*/node_modules"
+        "*/build"
+        "*/venv"
+        "*/.venv"
+      ];
+      repo = "ssh://borg@sagittarius/share/backup-spirit";
+      encryption = {
+        passCommand = "cat /etc/nixos/borg_passphrase";
+        mode = "repokey-blake2";
+      };
+      environment.BORG_RSH = "ssh -i /root/.ssh/id_ed25519";
+      startAt = "daily";
+      doInit = false;
+      prune.keep = {
+        daily = 10;
+        weekly = 7;
+        monthly = 4;
+        yearly = 12;
+      };
+    };
+
     hardware.video.hidpi.enable = true;
     environment.variables.GDK_DPI_SCALE = "1.5";
     services.xserver = {
