84cd769f5e879ad0b0a50512bbe939d5cd9d3aff
Alex O'Brien <3541@3541.website>
2022-08-01 11:38:14 +1000

    NIX[VM]: Enable scripts only on workstations.

--- nixos/src/home/util/vrun.nix
+++ nixos/src/home/util/vrun.nix
@@ -1,5 +1,5 @@
 { cfg, lib, pkgs, ... }: {
-  config.home = lib.mkIf (cfg.enable && cfg.home.enable) {
+  config.home = lib.mkIf (cfg.enable && cfg.home.enable && cfg.role == "workstation") {
     packages =
       [ (pkgs.writeShellScriptBin "vrun" (builtins.readFile ./vrun.sh)) ];
   };
