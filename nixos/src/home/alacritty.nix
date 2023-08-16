532f38286f4b96570f719bd138cb6abef8f66b27
Alex O'Brien <alex.obrien@imc.com>
2023-08-16 20:04:59 +1000

    NIX[ALACRITTY]: Add tmux keybindings.

--- nixos/src/home/alacritty.nix
+++ nixos/src/home/alacritty.nix
@@ -5,6 +5,7 @@
 
       settings = {
         cursor.style.shape = "Underline";
+        window.option_as_alt = "Both";
 
         colors = {
           primary = {
@@ -39,6 +40,117 @@
           normal.family = cfg.home.ui.fonts.editor.font;
           size = cfg.home.ui.fonts.editor.size;
         };
+
+        key_bindings = lib.optionals (cfg.platform == "darwin") [
+          {
+            # Vertical split.
+            mods = "Command";
+            key = "D";
+            chars = "\\x02\\x25";
+          }
+          {
+            # Horizontal split.
+            mods = "Command|Shift";
+            key = "D";
+            chars = "\\x02\\x22";
+          }
+          {
+            # Close pane.
+            mods = "Command";
+            key = "W";
+            chars = "\\x02\\x78";
+          }
+          {
+            # Open tab.
+            mods = "Command";
+            key = "T";
+            chars = "\\x02\\x63";
+          }
+          {
+            # Move left.
+            mods = "Command";
+            key = "H";
+            chars = "\\x02\\x1b\\x5b\\x44";
+          }
+          {
+            # Move down.
+            mods = "Command";
+            key = "J";
+            chars = "\\x02\\x1b\\x5b\\x42";
+          }
+          {
+            # Move up.
+            mods = "Command";
+            key = "K";
+            chars = "\\x02\\x1b\\x5b\\x41";
+          }
+          {
+            # Move right.
+            mods = "Command";
+            key = "L";
+            chars = "\\x02\\x1b\\x5b\\x43";
+          }
+          {
+            # Go to tab 0.
+            mods = "Command";
+            key = "Key0";
+            chars = "\\x02\\x30";
+          }
+          {
+            # Go to tab 1.
+            mods = "Command";
+            key = "Key1";
+            chars = "\\x02\\x31";
+          }
+          {
+            # Go to tab 2.
+            mods = "Command";
+            key = "Key2";
+            chars = "\\x02\\x32";
+          }
+          {
+            # Go to tab 3.
+            mods = "Command";
+            key = "Key3";
+            chars = "\\x02\\x33";
+          }
+          {
+            # Go to tab 4.
+            mods = "Command";
+            key = "Key4";
+            chars = "\\x02\\x34";
+          }
+          {
+            # Go to tab 5.
+            mods = "Command";
+            key = "Key5";
+            chars = "\\x02\\x35";
+          }
+          {
+            # Go to tab 6.
+            mods = "Command";
+            key = "Key6";
+            chars = "\\x02\\x36";
+          }
+          {
+            # Go to tab 7.
+            mods = "Command";
+            key = "Key7";
+            chars = "\\x02\\x37";
+          }
+          {
+            # Go to tab 8.
+            mods = "Command";
+            key = "Key8";
+            chars = "\\x02\\x38";
+          }
+          {
+            # Go to tab 9.
+            mods = "Command";
+            key = "Key9";
+            chars = "\\x02\\x39";
+          }
+        ];
       };
     };
 }
