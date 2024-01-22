865341c06bb4a0fce1c1fa3209f87c44a80bd6e5
Alex O'Brien <alex.obrien@imc.com>
2024-01-22 21:40:17 +1100

    NIX[EMACS]: Set path from shell on Darwin.

--- nixos/src/home/emacs.nix
+++ nixos/src/home/emacs.nix
@@ -714,9 +714,20 @@
                 toString cfg.home.ui.fonts.editor.size
               }"))
           '' + lib.optionalString (cfg.platform == "darwin") ''
-            (add-to-list 'exec-path "~/.nix-profile/bin")
-            (add-to-list 'exec-path "/nix/var/nix/profiles/default/bin")
-            (add-to-list 'exec-path "/usr/local/bin")
+            (defun set-exec-path-from-shell-PATH ()
+              "Set up Emacs' `exec-path' and PATH environment variable to match
+              that used by the user's shell.
+
+              This is particularly useful under Mac OS X and macOS, where GUI
+              apps are not started from a shell."
+              (interactive)
+              (let ((path-from-shell (replace-regexp-in-string
+                                       "[ \t\n]*$" "" (shell-command-to-string
+                                                       "bash --login -c 'echo $PATH'"))))
+               (setenv "PATH" path-from-shell)
+               (setq exec-path (split-string path-from-shell path-separator))))
+
+            (set-exec-path-from-shell-PATH)
           '';
         };
       };
